4248200ac6686767d35ff90586ef0009b166e864
tblais1224 <thomasblais.student@careerdevs.com>
2019-04-19 16:46:11 -0400

    seekAndDestroy added

---
+++ intermediateAlgorithm/seekAndDestroy.js
@@ -0,0 +1,19 @@
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+
+
+function destroyer(arr) {
+    // Remove all the values
+    var newArr = arr;
+    for (var j = 0; j < arr.length; j++) {
+        for (let i = 1; i < arguments.length; i++) {
+            if (arr[j] == arguments[i]) {
+                newArr.splice(j, 1);
+                j--;
+            };
+        }
+    }
+    return newArr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
