c68a54975f47bc898926f60418177d03ab799083
tblais1224 <thomasblais.student@careerdevs.com>
2019-06-17 22:42:00 -0400

    working on recursion heaps algorithm

---
+++ noRepeatsPlease.js
@@ -0,0 +1,33 @@
+// Algorithms: No Repeats Please
+
+// Return the number of total permutations of the provided string that don't have repeated consecutive letters. Assume that all characters in the provided string are each unique.
+
+// For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.
+
+
+function permAlone(str) {
+    let result = 0
+    let n = str.length
+    let arrStart = str.split()
+    function permutation(n, arrStart) {
+        if (n == 1) {
+            console.log(arr);
+            return;
+        }
+
+    }
+    return result;
+}
+
+permAlone('aab');
+
+// permAlone("aab") should return a number.
+// permAlone("aab") should return 2.
+// permAlone("aaa") should return 0.
+// permAlone("aabb") should return 8.
+// permAlone("abcdefa") should return 3600.
+// permAlone("abfdefa") should return 2640.
+// permAlone("zzzzzzzz") should return 0.
+// permAlone("a") should return 1.
+// permAlone("aaab") should return 0.
+// permAlone("aaabb") should return 12.
