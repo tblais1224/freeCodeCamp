6f9e9ff8aaa06895c608b56500798361c9267c29
tblais1224 <thomasblais.student@careerdevs.com>
2019-10-22 22:39:06 -0400

    splinal case done

--- timedAlgs/spinalTapCase.js
+++ timedAlgs/spinalTapCase.js
@@ -1,9 +1,20 @@
 //Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
 function spinalCase(str) {
-
-    return str;
+  let spinal = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " " || str[i] === "_" || str[i] === "-") {
+      spinal += "-";
+    } else if (str[i] === str[i].toUpperCase() && i !== 0 && spinal[spinal.length - 1] !== '-') {
+      spinal += "-" + str[i];
+    } else {
+      spinal += str[i];
+    }
   }
-  
-  spinalCase('This Is Spinal Tap');
-  
+  console.log(spinal.toLowerCase());
+  return spinal.toLowerCase();
+}
+
+spinalCase("This Is Spinal Tap");
+
+//finished in 12 min
